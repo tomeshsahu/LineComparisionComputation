20695c90ab56fe016ef45fabfe03c44277b4ebb2
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-09 19:44:27 +0530

    [TOMESHWAR]ADD:- LINE COMPARISION CARTESION SYSTEM

---
+++ LineComparision/LineComp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class LineComp
+    {
+        int x1, x2, y1, y2;
+        public LineComp(int x1, int x2, int y1, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        public void CalculateLength()
+        {
+            double result=Math.Sqrt((x2-x1)^2+(y2-y1)^2);
+            Console.WriteLine("Length of the Cartesion Line :"+" "+result);
+        }
+    }
+}
---
+++ LineComparision/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+namespace LineComparision;
+
+class program
+{
+    public static void Main(String[] args)
+    {
+        LineComp Comparision = new LineComp(2,5,6,8);
+        Comparision.CalculateLength();
+    }
+}
